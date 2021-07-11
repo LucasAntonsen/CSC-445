db727ec1a20f0476356f54cb2a00281bfc925fb8
LucasAntonsen <66390364+LucasAntonsen@users.noreply.github.com>
2021-07-11 11:24:44 -0700

    tableau built

--- src/simplex.java
+++ src/simplex.java
@@ -12,6 +12,7 @@ public class simplex {
 
     public static void fill_Table(File input, float[][] table, int num_rows) {
         int row = 0;
+        int length = 0;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
 
@@ -20,9 +21,19 @@ public class simplex {
                 String line = reader.readLine();
                 String[] str = line.split("\\s+");
 
-                for(int i = 0; i < str.length; i++){
-                    table[row][i] = Float.parseFloat(str[i]);
+                if(row == 0){
+                    table[0][0] = 0;
+                    length = str.length;
+
+                }else{
+                    table[row][0] = Float.parseFloat(str[str.length-1]);
+                    length = str.length - 1;
                 }
+
+                for(int i = 0; i < length; i++){
+                    table[row][i+1] = Float.parseFloat(str[i]);
+                }
+
                 row++;
             }
         } catch (IOException e) {
@@ -56,13 +67,25 @@ public class simplex {
         return num_cols;
     }
 
-    public static void print_Table(float[][] table, int num_rows){
+    public static void print_Table(float[][] table, String[] row_labels, String[] col_labels, int num_rows){
         for(int i = 0; i < num_rows; i++){
+            System.out.print(row_labels[i] + " = ");
             for(int y = 0; y < table[i].length; y++){
-                System.out.print(table[i][y] + " ");
+                if(y == 0){
+                    System.out.print(table[i][y] + " + ");
+                }else if(y == table[i].length - 1){
+                    System.out.print(table[i][y] + col_labels[y]);
+                }else{
+                    System.out.print(table[i][y] + col_labels[y] + " + ");
+                }
             }
             System.out.println();
         }
+    }
+
+    public static int[] gen(){
+        int[] a = {1,2,3};
+        return a;
     }
 
     public static void main(String[] args){
@@ -71,16 +94,37 @@ public class simplex {
         int rows = row_Count(inFile);
         int cols = col_Count(inFile);
 
+        int i = 0;
+
         System.out.println("Number of rows: " + rows + " Number of columns: " + cols);
 
         float[][] table = new float[rows][];
 
-        for(int i = 0; i < rows; i++){
+        for(i = 0; i < rows; i++){
             table[i] = new float[cols];
         }
         //System.out.println(table[0][0]);
 
         fill_Table(inFile, table, rows);
-        print_Table(table, rows);
+
+        String[] col_labels = new String[cols];
+        col_labels[0] = "ob";
+        for(i = 1; i < cols; i++){
+            col_labels[i] = "x" + i;
+        }
+        System.out.println(Arrays.toString(col_labels));
+
+        String[] row_labels = new String[rows];
+        row_labels[0] = "fi";
+        for(i = 1; i < rows; i++){
+            row_labels[i] = "w" + i;
+        }
+        System.out.println(Arrays.toString(row_labels));
+
+        print_Table(table, row_labels, col_labels, rows);
+
+        //generate_Dictionary();
+        int[] x = gen();
+        System.out.println(Arrays.toString(x));
     }
 }
